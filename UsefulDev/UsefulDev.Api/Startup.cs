3d2e3d3ff289e7d80c69bcd59ace74d23c5e7242
Vinicius Lelis <ViniciusLelis@users.noreply.github.com>
2021-06-20 13:18:47 -0300

    Adding swagger to api

--- UsefulDev/UsefulDev.Api/Startup.cs
+++ UsefulDev/UsefulDev.Api/Startup.cs
@@ -3,31 +3,77 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace UsefulDev.Api
 {
+    /// <summary>
+    /// Api startup
+    /// </summary>
     public class Startup
     {
+        const string _appTitle = "UsefulDev";
+        const string _appDescription = "Tools for developers and QA analysts";
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Configuration
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container. 
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
+
             services.AddControllers();
+
+            services.AddHealthChecks();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.OrderActionsBy((apiDesc) => apiDesc.RelativePath);
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = _appTitle,
+                    Description = _appDescription,
+                    Version = "v1"
+                });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UsePathBase("/useful-dev");
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", _appTitle);
+            });
 
             app.UseHttpsRedirection();
 
@@ -38,6 +84,7 @@ namespace UsefulDev.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
